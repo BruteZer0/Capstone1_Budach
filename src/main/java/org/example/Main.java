1812da2e2c0c6d78233914baeeb7e24665e3550a
BruteZer0 <uybudach08@gmail.com>
2026-04-29 12:04:16 -0400

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -9,67 +11,131 @@ public class Main {
         List<Transaction> transactions = TransactionManager.getTransactions();
 
         while (true) {
-            System.out.println("\n=== MENU ===");
+            System.out.println("\n=== MAIN MENU ===");
             System.out.println("D) Add Deposit");
-            System.out.println("P) Make Payment");
-            System.out.println("L) Ledger");
+            System.out.println("P) Make Payment (Debit)");
+            System.out.println("L) Show Ledger");
             System.out.println("X) Exit");
             System.out.print("Choose an option: ");
-
 
             char menuChoice = getValidChar(scanner);
 
             switch(menuChoice) {
                 case 'D':
-                    System.out.println("\n=== MAKE DEPOSIT ===");
+                    deposit(transactions);
                     break;
 
                 case 'P':
-                    System.out.println("\n=== MAKE PAYMENT  ===");
+                    payment(transactions);
                     break;
 
                 case 'L':
-                    System.out.println("\n=== LEDGER ===");
-                    System.out.println("A) All");
-                    System.out.println("D) Deposits");
-                    System.out.println("P) Payments");
-                    System.out.println("R) Reports");
-                    System.out.println("H) Home");
-                    System.out.print("Choose an option: ");
-
-                    char ledgerChoice = getValidChar(scanner);
-
-                    switch(ledgerChoice) {
-                        case 'A':
-                            displayTransactions(transactions);
-                            break;
-                        case 'D':
-                        case 'P':
-                        case 'R':
-                        case 'H':
-                            break;
-
-                    }
-                    break;
-
-
+                     ledger(transactions);
+                     break;
 
                 case 'X':
-                    System.out.println("\nExiting system");
-                    Thread.sleep(1000);
-                    System.out.println("Goodbye");
-                    System.exit(0);
-
+                    mainMenuExit(transactions);
+                    break;
             }
-
-
         }
+    }
 
+    public static void deposit(List<Transaction> transactions){
+        System.out.println("\n=== MAKE DEPOSIT ===");
+    }
+
+    public static void payment(List<Transaction> transactions){
+        System.out.println("\n=== MAKE PAYMENT  ===");
+    }
+
+    public static void ledger(List<Transaction> transactions){
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("\n=== LEDGER ===");
+            System.out.println("A) All");
+            System.out.println("D) Show Deposits");
+            System.out.println("P) Show Payments");
+            System.out.println("R) Show Reports");
+            System.out.println("H) Home");
+            System.out.print("Choose an option: ");
+
+            char ledgerChoice = getValidChar(scanner);
+
+            switch (ledgerChoice) {
+                case 'A':
+                    System.out.println("\nShowing Entries:");
+                    displayTransactions(transactions);
+                    break;
+                case 'D':
+                    System.out.println("\nShowing Entries:");
+                    displayDeposits(transactions);
+                    break;
+                case 'P':
+                    System.out.println("\nShowing Entries:");
+                    displayPayments(transactions);
+                    break;
+                case 'R':
+                    reports(transactions);
+                    break;
+                case 'H':
+                    return;
+            }
+        }
+    }
+
+    public static void mainMenuExit(List<Transaction> transactions) throws InterruptedException {
+        System.out.println("\nExiting system...");
+        Thread.sleep(1000);
+        System.out.println("Goodbye!");
+        System.exit(0);
+    }
+
+    public static void reports(List<Transaction> transactions){
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n=== REPORTS ===");
+            System.out.println("1) Month to Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search By Vendor");
+            System.out.println("6) Back");
+            System.out.print("Choose an option: ");
+
+            int reportChoice = getValidInt(scanner);
+
+            switch (reportChoice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("\nShowing Vendors:");
+                    displayVendors(transactions);
+                    System.out.print("\nType Vendor Name: ");
+                    String vendorName = scanner.nextLine();
+                    System.out.println("\nShowing Entries:");
+                    for (Transaction transaction : transactions) {
+                        if (Objects.equals(transaction.getVendor(), vendorName)) {
+                            System.out.println(transaction.toString());
+                        }
+                    }
+                    break;
+                case 6:
+                    return;
+            }
+        }
     }
 
     public static char getValidChar(Scanner scanner) {
         while (true) {
-            String input = scanner.next().trim();
+            String input = scanner.nextLine().trim();
 
             if (input.length() == 1 && Character.isLetter(input.charAt(0))) {
                 return Character.toUpperCase(input.charAt(0));
@@ -77,10 +143,51 @@ public class Main {
 
             System.out.print("Invalid input. Enter a single letter: ");
         }
-    }
+    }//validation input for character input
+
+    public static int getValidInt(Scanner scanner) {
+        while (true) {
+            String input = scanner.nextLine();
+
+            try {
+                return Integer.parseInt(input);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number.");
+            }
+        }
+    }//validation input for integer input
 
     public static void displayTransactions(List<Transaction> transactions){
         for(Transaction transaction: transactions)
             System.out.println(transaction.toString());
+    }
+
+    public static void displayDeposits(List<Transaction> transactions){
+        for(Transaction transaction: transactions){
+            if(transaction.getAmount() > 0){
+                System.out.println(transaction);
+            }
+        }
+    }
+
+    public static void displayPayments(List<Transaction> transactions){
+        for(Transaction transaction: transactions){
+            if(transaction.getAmount() < 0){
+                System.out.println(transaction);
+            }
+        }
+    }
+
+    public static void displayVendors(List<Transaction> transactions){
+        List<String> seen = new ArrayList<>();
+
+        for (Transaction transaction : transactions) {
+            String vendor = transaction.getVendor();
+
+            if (!seen.contains(vendor)) {
+                seen.add(vendor);
+                System.out.println(vendor);
+            }
+        }
     }
 }
